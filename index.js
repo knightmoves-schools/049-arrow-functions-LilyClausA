87bf7e9513f66ba78675c3eaeb572c617ee47806
LilyClausA <lilcla1230@students.maquoketaschools.org>
2026-01-28 20:29:07 +0000

    drtcfvygbuhylh

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+let add = function (a, b){
+    return a + b
+};
 
+document.getElementById("result").innerHTML = add;
+
+
+//should create a variable named add and assign it an arrow function 
+//that takes two number parameters and returns their sum
